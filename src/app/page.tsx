f862abca1a9e8f99eb65e5c98c4a18a6bcc9516e
lime999 <104721492+lime999@users.noreply.github.com>
2025-09-13 12:52:16 +0200

    tic tac toe start

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+import { useState } from 'react';
+
+export default function Board() {
+  return (
+    <>
+      <div className='board-row'>
+        <button className='pin'></button>
+        <button className='pin'></button>
+        <button className='pin'></button>
+        <button className='pin'></button>
+      </div>
+      
+    </>
+  );
+}
