67ebc74fb3d88dc537c74a5873080c26d4c89189
Tobias <1056318+tobias-gaenzler@users.noreply.github.com>
2025-11-23 16:35:43 +0100

    adding popup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { exampleColors } from "./exampleColors";
 import { generateSolution } from "./generateSolution";
 import { checkSolution } from "./checkSolution";
 import { Row } from "./row";
+import { Popup } from "./popup";
 
 export default function Board() {
   const [solution, setSolution] = useState(generateSolution);
@@ -12,10 +13,13 @@ export default function Board() {
     Array(10).fill(null).map(() => [0, 0, 0, 0])
   );
 
+  const [popupVisible, setPopupVisible] = useState(false);
+
   const [alertMessage, setAlertMessage] = useState("");
   const [round, setRound] = useState(1);
 
   const handleColorChange = (rowId: number, pinId: number, newColor: number) => {
+    setPopupVisible(true);
     setRowColors(prev => {
       const updated = prev.map((row, idx) =>
         idx === rowId
@@ -28,30 +32,41 @@ export default function Board() {
 
 
   return (
-    <div className="game">
-      <div>
-        {rowColors.map((rowColors, rowId) => (
-          <Row
-            key={rowId}
-            rowId={rowId}
-            pinColors={rowColors}
-            enabled={round - 1 === rowId}
-            onColorChange={handleColorChange}
-          />
-        ))}
-      </div>
-      
-      <div className="side-panel">
-        <button
-          className="check-solution-btn"
-          onClick={() => checkSolution(rowColors[round - 1], solution, round, setAlertMessage, setRound)}
-        >
-          Verify guess
-        </button>
-        <h2>{alertMessage}</h2>
-        <h1>Round {round}/10</h1>
-        <h1>These are the allowed colors:</h1>
-        <div>{exampleColors()}</div>
+    <div>
+      {popupVisible && (
+        <Popup
+          onColorSelect={(newColor) => {
+            // Here you would handle the color selection for the currently active pin
+            setPopupVisible(false);
+          }
+          }
+          handlePopupClose={() => setPopupVisible(false)}
+        />)}
+      <div className="game">
+        <div>
+          {rowColors.map((rowColors, rowId) => (
+            <Row
+              key={rowId}
+              rowId={rowId}
+              pinColors={rowColors}
+              enabled={round - 1 === rowId}
+              onColorChange={handleColorChange}
+            />
+          ))}
+        </div>
+
+        <div className="side-panel">
+          <button
+            className="check-solution-btn"
+            onClick={() => checkSolution(rowColors[round - 1], solution, round, setAlertMessage, setRound)}
+          >
+            Verify guess
+          </button>
+          <h2>{alertMessage}</h2>
+          <h1>Round {round}/10</h1>
+          <h1>These are the allowed colors:</h1>
+          <div>{exampleColors()}</div>
+        </div>
       </div>
     </div>
   );
