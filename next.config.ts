236c5a9ae3159f4b3f4d32a021656995f535073c
lime999 <104721492+lime999@users.noreply.github.com>
2026-01-03 13:24:59 +0100

    Update configuration for deployment and add touch-cli dependency

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/mastermind-react',
+  assetPrefix: '/mastermind-react',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
